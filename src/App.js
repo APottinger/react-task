0701d35769bc19c84849195c02d2fd99435d9924
Allen Pottinger <allenpottinger@gmail.com>
2021-12-21 20:23:33 -0500

    adds global state to app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+    const [tasks, setTasks] = useState([
+      {
+          id: 1,
+          text: 'Doctors Appt',
+          day: 'Feb 5th at 2:30pm',
+          reminder: true
+      }, 
+      {
+          id: 2,
+          text: 'Meeting PTA',
+          day: 'Feb 11th at 8:3apm',
+          reminder: false
+      },
+      {
+          id: 3,
+          text: 'Conference Board Meeting',
+          day: 'Feb 8th at 10:00am',
+          reminder: true
+      }
+    ]);
+
+    //DELETE A TASK 
+    const deleteTask = (id) => {
+      setTasks(tasks.filter(task => task.id !== id))
+    }
+
+    //TOGGLE REMINDER
+    const toggleReminder = (id) => {
+      setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder } : task))
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Keep Moving Forward'/>
+      
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} toggleReminder={toggleReminder}/> : 'You have no tasks...'}
     </div>
   );
 }
