5bb7c94c5753182e7567c4b3597ae1990e065cac
Allen Pottinger <allenpottinger@gmail.com>
2021-12-21 20:25:03 -0500

    adds Navbar Component

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import NavItem from './NavItem'
+
+const NavBar = () => {
+    //create a ul
+    //link
+
+    return (
+        <div>
+            <ul>
+                <NavItem title='Home'/>
+                <NavItem title='About'/>
+                <NavItem title='Contact'/>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
